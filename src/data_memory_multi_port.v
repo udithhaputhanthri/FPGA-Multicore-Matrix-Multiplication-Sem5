c797e86cf837d46acdd964ea315047a0730cbb1e
Shalutha <shalutha321@gmail.com>
2021-07-07 04:58:43 +0530

    working code for 10 cores tested up to 16X16 Matrix

--- src/data_memory_multi_port.v
+++ src/data_memory_multi_port.v
@@ -1,9 +1,9 @@
 module data_memory_multi_port
 (
-	input [15:0] data_in_1, data_in_2, data_in_3, data_in_4,
-	input [15:0] addr_1, addr_2, addr_3, addr_4,
-	input we_1, we_2, we_3, we_4, clk,
-	output [15:0] data_out_1, data_out_2, data_out_3, data_out_4
+	input [15:0] data_in_1, data_in_2, data_in_3, data_in_4, data_in_5, data_in_6, data_in_7, data_in_8, data_in_9, data_in_10,
+	input [15:0] addr_1, addr_2, addr_3, addr_4, addr_5, addr_6, addr_7, addr_8, addr_9, addr_10, 
+	input we_1, we_2, we_3, we_4, we_5, we_6, we_7, we_8, we_9, we_10, clk,
+	output [15:0] data_out_1, data_out_2, data_out_3, data_out_4, data_out_5, data_out_6, data_out_7, data_out_8, data_out_9, data_out_10
 );
 	
 // Declare the RAM variable
@@ -13,6 +13,13 @@ assign data_out_1 = ram[addr_1];
 assign data_out_2 = ram[addr_2];
 assign data_out_3 = ram[addr_3];
 assign data_out_4 = ram[addr_4];
+assign data_out_5 = ram[addr_5];
+assign data_out_6 = ram[addr_6];
+assign data_out_7 = ram[addr_7];
+assign data_out_8 = ram[addr_8];
+assign data_out_9 = ram[addr_9];
+assign data_out_10 = ram[addr_10];
+
 
 always @ (posedge clk)
 	begin
@@ -20,6 +27,13 @@ always @ (posedge clk)
 		if (we_2) ram[addr_2] <= data_in_2;
 		if (we_3) ram[addr_3] <= data_in_3;
 		if (we_4) ram[addr_4] <= data_in_4;
+		if (we_5) ram[addr_5] <= data_in_5;
+		if (we_6) ram[addr_6] <= data_in_6;
+		if (we_7) ram[addr_7] <= data_in_7;
+		if (we_8) ram[addr_8] <= data_in_8;
+		if (we_9) ram[addr_9] <= data_in_9;
+		if (we_10) ram[addr_10] <= data_in_10;
+		
 	end
 	
 endmodule
--- src/top.v
+++ src/top.v
@@ -23,20 +23,50 @@ wire [15:0] imem_in;
 wire [15:0] dmem_out;
 wire [15:0] ar_out;
 
-// Intermediate wires for core 1
+// Intermediate wires for core 2
 wire [15:0] dmem_in2;
 wire [15:0] dmem_out2;
 wire [15:0] ar_out2;
 
-// Intermediate wires for core 1
+// Intermediate wires for core 3
 wire [15:0] dmem_in3;
 wire [15:0] dmem_out3;
 wire [15:0] ar_out3;
 
-// Intermediate wires for core 1
+// Intermediate wires for core 4
 wire [15:0] dmem_in4;
 wire [15:0] dmem_out4;
 wire [15:0] ar_out4;
+
+// Intermediate wires for core 5
+wire [15:0] dmem_in5;
+wire [15:0] dmem_out5;
+wire [15:0] ar_out5;
+
+// Intermediate wires for core 6
+wire [15:0] dmem_in6;
+wire [15:0] dmem_out6;
+wire [15:0] ar_out6;
+
+// Intermediate wires for core 7
+wire [15:0] dmem_in7;
+wire [15:0] dmem_out7;
+wire [15:0] ar_out7;
+
+// Intermediate wires for core 8
+wire [15:0] dmem_in8;
+wire [15:0] dmem_out8;
+wire [15:0] ar_out8;
+
+// Intermediate wires for core 9
+wire [15:0] dmem_in9;
+wire [15:0] dmem_out9;
+wire [15:0] ar_out9;
+
+// Intermediate wires for core 10
+wire [15:0] dmem_in10;
+wire [15:0] dmem_out10;
+wire [15:0] ar_out10;
 
 
 //Control signals
@@ -45,6 +75,12 @@ wire read_MD, read_MI, write_MD;    //core 1
 wire read_MD2, read_MI2, write_MD2; //core 2
 wire read_MD3, read_MI3, write_MD3; //core 3
 wire read_MD4, read_MI4, write_MD4; //core 4
+wire read_MD5, read_MI5, write_MD5; //core 5
+wire read_MD6, read_MI6, write_MD6; //core 6
+wire read_MD7, read_MI7, write_MD7; //core 7
+wire read_MD8, read_MI8, write_MD8; //core 8
+wire read_MD9, read_MI9, write_MD9; //core 9
+wire read_MD10, read_MI10, write_MD10; //core 10
 
 //Used to write data in data memory to an external file
 assign dmem_out_disp = dmem_in;
@@ -88,7 +124,7 @@ tb_mux_mem_data mem_data_select_mux(
 );
 
 //Allowing all cores to access the instructions
-assign read_MI_F = read_MI || read_MI2 || read_MI3 || read_MI4;
+assign read_MI_F = read_MI || read_MI2 || read_MI3 || read_MI4 || read_MI5 || read_MI6 || read_MI7 || read_MI8 ||read_MI9 || read_MI10 ;
 
 //Initiating Cores
 // [CORE 1]
@@ -145,6 +181,84 @@ core #(.core_id(3)) processor4 (
     .ar_out(ar_out4)
  );
 
+ // [CORE 5]
+core #(.core_id(4)) processor5 (
+    .clk(clk), 
+    .START(START), 
+    .RESET(RESET), 
+    .read_MI(read_MI5), 
+    .imem_in(imem_in),
+    .dmem_in(dmem_in5), 
+    .dmem_out(dmem_out5), 
+    .write_MD(write_MD5), 
+    .ar_out(ar_out5)
+ );
+
+  // [CORE 6]
+core #(.core_id(5)) processor6 (
+    .clk(clk), 
+    .START(START), 
+    .RESET(RESET), 
+    .read_MI(read_MI6), 
+    .imem_in(imem_in),
+    .dmem_in(dmem_in6), 
+    .dmem_out(dmem_out6), 
+    .write_MD(write_MD6), 
+    .ar_out(ar_out6)
+ );
+
+   // [CORE 7]
+core #(.core_id(6)) processor7 (
+    .clk(clk), 
+    .START(START), 
+    .RESET(RESET), 
+    .read_MI(read_MI7), 
+    .imem_in(imem_in),
+    .dmem_in(dmem_in7), 
+    .dmem_out(dmem_out7), 
+    .write_MD(write_MD7), 
+    .ar_out(ar_out7)
+ );
+
+  // [CORE 8]
+core #(.core_id(7)) processor8 (
+    .clk(clk), 
+    .START(START), 
+    .RESET(RESET), 
+    .read_MI(read_MI8), 
+    .imem_in(imem_in),
+    .dmem_in(dmem_in8), 
+    .dmem_out(dmem_out8), 
+    .write_MD(write_MD8), 
+    .ar_out(ar_out8)
+ );
+
+
+  // [CORE 9]
+core #(.core_id(8)) processor9 (
+    .clk(clk), 
+    .START(START), 
+    .RESET(RESET), 
+    .read_MI(read_MI9), 
+    .imem_in(imem_in),
+    .dmem_in(dmem_in9), 
+    .dmem_out(dmem_out9), 
+    .write_MD(write_MD9), 
+    .ar_out(ar_out9)
+ );
+
+   // [CORE 10]
+core #(.core_id(9)) processor10 (
+    .clk(clk), 
+    .START(START), 
+    .RESET(RESET), 
+    .read_MI(read_MI10), 
+    .imem_in(imem_in),
+    .dmem_in(dmem_in10), 
+    .dmem_out(dmem_out10), 
+    .write_MD(write_MD10), 
+    .ar_out(ar_out10)
+ );
 // [Data Memory]
 
 // for single core
@@ -174,7 +288,33 @@ data_memory_multi_port DM_4(
     .we_4(write_MD4), 
     .addr_4(ar_out4), 
     .data_in_4(dmem_out4), 
-    .data_out_4(dmem_in4)
+    .data_out_4(dmem_in4),
+    .we_5(write_MD5), 
+    .addr_5(ar_out5), 
+    .data_in_5(dmem_out5), 
+    .data_out_5(dmem_in5),
+    .we_6(write_MD6), 
+    .addr_6(ar_out6), 
+    .data_in_6(dmem_out6), 
+    .data_out_6(dmem_in6),
+    .we_7(write_MD7), 
+    .addr_7(ar_out7), 
+    .data_in_7(dmem_out7), 
+    .data_out_7(dmem_in7),
+    .we_8(write_MD8), 
+    .addr_8(ar_out8), 
+    .data_in_8(dmem_out8), 
+    .data_out_8(dmem_in8),
+    .we_9(write_MD9), 
+    .addr_9(ar_out9), 
+    .data_in_9(dmem_out9), 
+    .data_out_9(dmem_in9),
+    .we_10(write_MD10), 
+    .addr_10(ar_out10), 
+    .data_in_10(dmem_out10), 
+    .data_out_10(dmem_in10)
+
+
 );
 
 
